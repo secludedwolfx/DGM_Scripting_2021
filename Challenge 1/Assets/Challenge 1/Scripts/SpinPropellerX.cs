a3e501d7d02a6bf06457eba97a1f5089a9871950
Josie McNeil <10889549@my.uvu.edu>
2021-02-01 21:42:13 -0700

    Complete Challenge 1 Plane

---
+++ Challenge 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    private float rotationSpeed = 100.0f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
