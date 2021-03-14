3ee0100d4f46a793266a8ac140f42f3070b571bb
Josie McNeil <10889549@my.uvu.edu>
2021-03-14 02:37:30 -0600

    Complete Prototype 2

---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{   //Animals are spawned and at random
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 15;
+    private float spawnposZ = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", 2, 1.5f); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnposZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+
+}
---
+++ Prototype 2/Assets/Scripts/moveforward.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveforward : MonoBehaviour
+{
+    public float speed = 40.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   //Soda moves forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
